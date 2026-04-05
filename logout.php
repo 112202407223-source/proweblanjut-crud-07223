c6b04ea1b80add6e8639f374947653e793712c92
Uliya Rizqiana <112202407223@mhs.dinus.ac.id>
2026-04-05 19:11:36 +0700

    tambah hapus cookie saat logout

--- logout.php
+++ logout.php
@@ -2,6 +2,12 @@
 // logout.php
 session_start();
 session_destroy();
+
+// Hapus cookie remember me
+if (isset($_COOKIE['remember_username'])) {
+    setcookie('remember_username', '', time() - 3600, '/');
+}
+
 header("Location: login.php?logout=1");
 exit();
 ?>
